d1de09b35354ece7381c4d530f3902cc7b74a27c
Alex Chen <alexander.chen@yale.edu>
2016-08-31 16:24:13 -0400

    Add initial header

--- voting-client/src/components/App.jsx
+++ voting-client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './header/Header'
 import '../stylesheets/main.css';
 
 export default React.createClass({
@@ -6,12 +7,7 @@ export default React.createClass({
   render: function() {
     return (
       <div className="container">
-        <div className="header">
-          <h1 className="logo">Leo Health</h1>
-          <div className="welcome">
-            <p>Welcome to Leo + Flatiron Pediatrics</p>
-          </div>
-        </div>
+        <Header/>
         <div className="body">
           <h1 className="sidebar">Leo Health</h1>
           <div className="main-body">
