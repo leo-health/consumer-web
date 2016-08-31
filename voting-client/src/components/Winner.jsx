50ee60047cf0fac4e41b8427b176759f5ea3a179
Alex Chen <alexander.chen@yale.edu>
2016-08-31 13:26:59 -0400

    Fix gitignore file

--- voting-client/src/components/Winner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Winner = React.createClass({
-  render: function() {
-    return <div ref="winner">Winner is {this.props.winner}</div>;
-  }
-});
-
-export default Winner;
