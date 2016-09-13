cdc58fa42a14993508a087ef4d62c5421985fe82
afanslau <afanslau@gmail.com>
2016-09-13 13:39:12 -0400

    isAuthenticated component wrapper works

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,12 +3,12 @@ import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger';
 import reducer from './reducers/reducer';
 
-export default function() {
+export default function configureStore() {
   return createStore(
     reducer,
     applyMiddleware(
       thunkMiddleware,
       createLogger()
     )
-  )
+  );
 }
