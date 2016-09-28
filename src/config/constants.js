c5ad7216d5ba7a1c07181abd16277d0bc981f2a0
afanslau <afanslau@gmail.com>
2016-09-28 12:20:27 -0400

    ngrok support for hot reloading

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,9 @@
-export const API_BASE_URL = "http://localhost:3000/api/v1"
+const LOCAL_API_SERVER = "http://localhost:3000";
+const NGROK_API_SERVER = "http://lapi.ngrok.io";
+
+const API_SERVER = NGROK_API_SERVER;
+
+export const API_BASE_URL = `${API_SERVER}/api/v1`;
 export const DATE_FORMATS = {
   STANDARD_DATE_WITH_SLASHES: "MM/DD/YYYY",
   DAY_OF_MONTH: "D",
@@ -7,4 +12,3 @@ export const DATE_FORMATS = {
   FULL_SPELLED_DATE: "dddd, MMMM Do"
 };
 export const API_DATE_FORMAT = DATE_FORMATS.STANDARD_DATE_WITH_SLASHES;
-// export const API_DATETIME_FORMAT =  // TODO: fill in
