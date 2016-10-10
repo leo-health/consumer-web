3c519ae00eb1b5e5f22a909b813e7826f4e80a47
afanslau <afanslau@gmail.com>
2016-10-10 17:14:20 -0400

    use localhost

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,7 +1,7 @@
 const LOCAL_API_SERVER = "http://localhost:3000";
 const NGROK_API_SERVER = "http://lapi.ngrok.io";
 
-const API_SERVER = NGROK_API_SERVER;
+const API_SERVER = LOCAL_API_SERVER;
 
 export const API_BASE_URL = `${API_SERVER}/api/v1`;
 export const DATE_FORMATS = {
